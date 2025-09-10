59ac6557b80846cff8d409a8aab7c5a3dc7df176
Serhii Propolov <job.tenshi@gmail.com>
2025-09-10 13:34:28 +0200

    add and edit icons

---
+++ client/src/shared/icons/ThemeIcon.tsx
@@ -0,0 +1,15 @@
+function ThemeIcon({ className }: { className: string }) {
+    return (
+        <svg viewBox="0 0 24 24" className={className}>
+            <g>
+                <path
+                    d="M9 6C9 10.9706 13.0294 15 18 15C18.9093 15 19.787 14.8655 20.6144 14.6147C19.4943 18.3103 16.0613 20.9999 12 20.9999C7.02944 20.9999 3 16.9707 3 12.0001C3 7.93883 5.69007 4.50583 9.38561 3.38574C9.13484 4.21311 9 5.09074 9 6Z"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                />
+            </g>
+        </svg>
+    );
+}
+
+export default ThemeIcon;
