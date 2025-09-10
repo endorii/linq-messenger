5cff9cbf01d83b1a0eb7d60de4baf1909fd0cf62
Serhii Propolov <job.tenshi@gmail.com>
2025-09-10 13:37:09 +0200

    init prisma

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,38 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
+import { ValidationPipe } from "@nestjs/common";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+import * as cookieParser from "cookie-parser";
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
+    app.setGlobalPrefix("api");
+    app.enableCors({
+        origin: "http://localhost:3000",
+        methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+        credentials: true,
+    });
+
+    app.use(cookieParser());
+
+    const config = new DocumentBuilder()
+        .setTitle("Mindset API")
+        .setDescription("API Docs")
+        .setVersion("1.0")
+        .addBearerAuth()
+        .build();
+
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup("docs", app, document);
+
+    app.useGlobalPipes(
+        new ValidationPipe({
+            transform: true,
+            whitelist: true,
+        })
+    );
+
     await app.listen(5000);
 }
+
 bootstrap();
