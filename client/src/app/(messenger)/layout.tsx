364593770b887eb889edd57cfbfbebb3679048ea
Serhii Propolov <job.tenshi@gmail.com>
2025-09-29 12:38:35 +0200

    added the ability to resize messenger windows

--- client/src/app/(messenger)/layout.tsx
+++ client/src/app/(messenger)/layout.tsx
@@ -4,6 +4,12 @@ import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 import { useProfile } from "@/features/auth/hooks/useAuth";
 import Sidebar from "@/features/sidebar/components/Sidebar";
+import {
+    ResizableHandle,
+    ResizablePanel,
+    ResizablePanelGroup,
+} from "@/shared/components/ui/resizable";
+import { LinqIcon } from "@/shared/icons";
 
 export default function MessengerLayout({
     children,
@@ -21,8 +27,8 @@ export default function MessengerLayout({
 
     if (isLoading) {
         return (
-            <div className="flex items-center justify-center w-screen h-screen text-white">
-                Loading...
+            <div className="flex items-center justify-center w-screen h-screen bg-neutral-950 text-white">
+                <LinqIcon className="w-[100px] h-[100px] animate-spin" />
             </div>
         );
     }
@@ -33,8 +39,13 @@ export default function MessengerLayout({
 
     return (
         <div className="flex w-screen h-screen bg-neutral-900 text-white">
-            <Sidebar user={user} />
-            <div className="flex-1">{children}</div>
+            <ResizablePanelGroup direction="horizontal">
+                <ResizablePanel minSize={15} maxSize={30} defaultSize={18}>
+                    <Sidebar user={user} />
+                </ResizablePanel>
+                <ResizableHandle />
+                <ResizablePanel defaultSize={72}>{children}</ResizablePanel>
+            </ResizablePanelGroup>
         </div>
     );
 }
