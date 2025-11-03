75c62d14b8597c564db65f3f734ff52606d2276f
Serhii Propolov <job.tenshi@gmail.com>
2025-11-03 12:33:07 +0100

    update

--- server/src/config/jwt-module.config.ts
+++ server/src/config/jwt-module.config.ts
@@ -1,14 +1,12 @@
 import { ConfigService } from "@nestjs/config";
-import { JwtModuleAsyncOptions, JwtModuleOptions } from "@nestjs/jwt";
-
-const jwtModuleOptions = (config: ConfigService): JwtModuleOptions => ({
-    secret: config.get<string>("ACCESS_TOKEN_SECRET"),
-    signOptions: {
-        expiresIn: config.get<string>("ACCESS_TOKEN_EXPIRES") || "15m",
-    },
-});
+import { JwtModuleAsyncOptions } from "@nestjs/jwt";
 
 export const jwtModuleAsyncOptions = (): JwtModuleAsyncOptions => ({
     inject: [ConfigService],
-    useFactory: (config: ConfigService) => jwtModuleOptions(config),
+    useFactory: (config: ConfigService) => ({
+        secret: config.get("ACCESS_TOKEN_SECRET"),
+        signOptions: {
+            expiresIn: config.get("ACCESS_TOKEN_EXPIRES") || "15m",
+        },
+    }),
 });
